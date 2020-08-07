6bfece62246b58ecb2e67e9b67d5f38f6653b56d
ACER <thimiraisiwara98@gmail.com>
2020-08-07 23:58:25 +0530

    Second Commit

--- app/src/main/java/com/example/tutorial_2/MainActivity.java
+++ app/src/main/java/com/example/tutorial_2/MainActivity.java
@@ -3,11 +3,13 @@ package com.example.tutorial_2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView textView2;
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +18,30 @@ public class MainActivity extends AppCompatActivity {
 
         textView2 = findViewById(R.id.textView2);
         textView2.setText(R.string.Msg2);
+        Log.i(TAG, "onCreate: ");
+
+        @Override
+        protected void onStart() {
+            super.onStart();
+            Log.i(TAG, "onStart: ");
+        }
+
+        @Override
+        protected void onStop() {
+            super.onStop();
+            Log.i(TAG, "onStop: ");
+        }
+
+        @Override
+        protected void onPostResume() {
+            super.onPostResume();
+            Log.i(TAG, "onPostResume: ");
+        }
+
+        @Override
+        protected void onRestart() {
+            super.onReStart();
+            Log.i(TAG, "onReStart: ");
+        }
     }
 }
